0c561201c1a9260d953cd22e3a601c7b351be65b
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-09-17 19:58:58 +0600

    save user info in mongodb

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,40 +1,101 @@
 import React, { use } from "react";
 import AuthContext from "../context/AuthContext";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
-    const {createUser} = use(AuthContext)
+  const { createUser } = use(AuthContext);
 
-    const handleSignUp = (e)=>{
-        e.preventDefault()
-        const form = e.target;
-        const formData = new FormData(form);
-        const email = formData.get('email');
-        const password = formData.get('password');
-        console.log(email, password)
-
-        // create user in the firebase 
-        createUser(email,password)
-        .then(result =>{
-            console.log(result)
-        })
-        .catch(error =>{
-            console.log(error)
-        })
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const { email, password, ...rest } = Object.fromEntries(formData.entries());
+    
+    const userProfile = {
+        email,
+        ...rest
     }
+    console.log(email, password, userProfile);
+
+    // create user in the firebase
+    createUser(email, password)
+      .then((result) => {
+        console.log(result);
+
+        // save the user profile Info  to DB
+        fetch("http://localhost:3000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userProfile),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+           
+            if (data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your account is Created",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="card bg-base-100  max-w-sm mx-auto mt-24 shrink-0 shadow-2xl">
-      <h1 className="text-5xl font-bold">Login now!</h1>
+      <h1 className="text-5xl font-bold">SIgnUp now!</h1>
       <div className="card-body">
         <form onSubmit={handleSignUp} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" className="input" name="name" placeholder="Name" />
+          <label className="label">Address</label>
+          <input
+            type="text"
+            className="input"
+            name="address"
+            placeholder="Address"
+          />
+          <label className="label">Phone</label>
+          <input
+            type="text"
+            className="input"
+            name="phone"
+            placeholder="Number"
+          />
+          <label className="label">Photo</label>
+          <input
+            type="text"
+            className="input"
+            name="photo"
+            placeholder="Photo URL"
+          />
+
           <label className="label">Email</label>
-          <input type="email" className="input" name="email" placeholder="Email" />
+          <input
+            type="email"
+            className="input"
+            name="email"
+            placeholder="Email"
+          />
           <label className="label">Password</label>
-          <input type="password" className="input" name="password" placeholder="Password" />
+          <input
+            type="password"
+            className="input"
+            name="password"
+            placeholder="Password"
+          />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button className="btn btn-neutral mt-4">SignUp</button>
         </form>
       </div>
     </div>
